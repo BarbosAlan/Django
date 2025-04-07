58966a980e94ee29f934b8f2905d561097b67f35
Alan Barbosa <149962269+BarbosAlan@users.noreply.github.com>
2025-04-07 16:20:49 -0300

    Update

--- contato/models.py
+++ contato/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Pessoa(models.Model):
+    id_pessoa = models.AutoField(primary_key=True)
+    nome = models.TextField(max_length=100)
+    email = models.EmailField(max_length=255)
+    assunto = models.TextField(max_length=300)
+    mensagem = models.TextField(max_length=300)
--- contato/views.py
+++ contato/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
 
+from contato.models import Pessoa
+
 # Create your views here.
+
+def cadastro(request):
+    contexto = {
+        'titulo' : 'Jornada Viagem | Cadastro',
+        'pessoas' : Pessoa.objects.all()
+    }
+    return render(request,
+                  'cadastro/index.html',
+                  contexto,)
+
+def gravar(request):
+    nova_pessoa = Pessoa()
+    nova_pessoa.nome = request.POST.get('nome')
+    nova_pessoa.email = request.POST.get('email')
+    nova_pessoa.assunto = request.POST.get('assunto')
+    nova_pessoa.mensagem = request.POST.get('mensagem')
+    nova_pessoa.save()
+    
+    return cadastro(request)
