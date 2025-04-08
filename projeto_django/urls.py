8a4c35b19a723568f678af9b925309361665e908
Alan Barbosa <149962269+BarbosAlan@users.noreply.github.com>
2025-04-08 13:26:59 -0300

    Update | Home

--- projeto_django/urls.py
+++ projeto_django/urls.py
@@ -4,7 +4,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('home', include('home.urls')),
+    path('home/', include('home.urls')),
     path('servicos/', include('servicos.urls')),
     path('sobre/', include('sobre.urls')),
     path('contato/', include('contato.urls')),
